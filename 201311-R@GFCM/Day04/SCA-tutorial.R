d704e80a389cd14a9f9adb8fe08a4335f91e183c
colinpmillar <colin.millar@jrc.ec.europa.eu>
2013-11-07 15:47:28 +0100

    SCA stuff

--- 201311-R@GFCM/Day04/SCA-tutorial.R
+++ 201311-R@GFCM/Day04/SCA-tutorial.R
@@ -16,9 +16,8 @@
 
 # load the libary and some data
 library(FLa4a)
-data(hakeGSA7)
-data(hakeGSA7.idx)
-
+data(ple4)
+data(ple4.indices)
 
 
 # ################################
@@ -36,18 +35,13 @@ data(hakeGSA7.idx)
 
 # YOu need to be a little familiar with formulas:
 # check out the details section in:
-?formula
+#?formula
 
 # and for the a4a model have a look at
-?a4aFit
+#?a4a
 
 
 # okay! now lets fit a stock assessment:
-
-
-# lets remind ourselves of the data we are going to use
-summary(hakeGSA7)
-summary(hakeGSA7.idx)
 
 # choose a model for log Fishing mortality
 fmodel <- ~ 1
@@ -57,24 +51,62 @@ fmodel <- ~ 1
 qmodel <- list( ~ 1 )
 
 # fit the model
-simplefit <- a4aFit(fmodel, qmodel, stock = hakeGSA7,  indices = hakeGSA7.idx)
+simplefit <- a4a(fmodel, qmodel, stock = ple4,  indices = ple4.indices[1], fit = "assessment")
 
 # look at a quick summary
-simplefit
+summary(simplefit)
 
 
 # lets look at the estimates of recruitment, Fbar, and SSB are
 
 # A good way is to add the predictions to the origional stock object this fill in stock.n and harvest
-simpleHake <- hakeGSA7 + simplefit
+ple4.simple <- ple4 + simplefit
 
 # plot the fitted stock object
-plot(simpleHake)
+plot(ple4.simple)
+
+# we can add in confidence intervals
+# in a4a we do this using simulation approach
+# Which works using a stock object with iters
+ple4.sim <- propagate(ple4, iter = 100)
+
+# and we add the stock assessment to this new stock object
+ple4.sim.fit <- ple4.sim + simplefit
 
 
+# lets do a quick comparison with default XSA
+library(FLXSA)
+ple4.xsa <- FLXSA(ple4, ple4.indices[1], FLXSA.control())
+ple4.xsa.fit <- ple4 + ple4.xsa
+
+# and plot using FLStocks
+plot(FLStocks(ple4.sim.fit, ple4.xsa.fit), col = c("black", "red"))
 
 
+#  Excercise
 
+# 1. try some different formulas for F
+# like:
+
+#  a) fmodel <- ~ year
+
+#  b) fmodel <- ~ s(year)
+
+#  c) fmodel <- ~ s(year) + age
+
+#  d) fmodel <- ~ s(year) + s(age)
+
+# by changing the code below:
+
+# set up the model
+fmodel <- ~ 1
+qmodel <- list( ~ 1 )
+
+# fit the model
+myfit <- a4a(fmodel, qmodel, stock = ple4,  indices = ple4.indices[1])
+
+# plot the fit
+plot(ple4 + myfit)
 
 
 
